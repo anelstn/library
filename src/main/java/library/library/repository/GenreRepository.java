5617357be3de0d814ed8f7e99ee6c7a7c7961554
nsbpq <anel.sultanbek@narxoz.kz>
2025-12-16 20:48:15 +0500

    Add repositories

---
+++ src/main/java/library/library/repository/GenreRepository.java
@@ -0,0 +1,9 @@
+package library.library.repository;
+
+import library.library.model.Genre;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GenreRepository extends JpaRepository<Genre, Long> {
+}
